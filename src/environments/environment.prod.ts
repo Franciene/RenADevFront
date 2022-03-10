e9e78571a582f5f69097068903eeaa157bf994de
Franciene Bernardi <bernardi.franciene@gmail.com>
2022-03-10 20:07:41 +0000

    fix environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCJaOT25yf-RWn_aneabVmV_4XvR86yifU",
+    authDomain: "rentadev-e1301.firebaseapp.com",
+    projectId: "rentadev-e1301",
+    storageBucket: "rentadev-e1301.appspot.com",
+    messagingSenderId: "440345170636",
+    appId: "1:440345170636:web:6a18d76f0a47c546626869"
+  }
 };
